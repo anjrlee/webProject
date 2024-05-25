6f8db0d47cf93d22c38449667ae39541e9e20229
alice20040127 <66763324+alice20040127@users.noreply.github.com>
2024-05-26 04:14:09 +0800

    Update Verify.vue

--- resources/js/components/verify/Verify.vue
+++ resources/js/components/verify/Verify.vue
@@ -11,28 +11,32 @@
         <div v-show="itemTable" class="showable">
           <p class="form__title"><strong>審核列表</strong></p>
           <hr class="form--separator">
-          <div class="table">
-            <div class="table__title">
-              <div class="table-cell">項目名稱</div>
-              <div class="table-cell">發表者</div>
-              <div class="table-cell">發表時間</div>
-              <div class="table-cell">狀態</div>
-            </div>
-            <div v-for="item in filteredItems" :key="item.id" class="table-row" @click="showItemcontent(item)">
-              <div class="table-cell">{{ item.title }}</div>
-              <div class="table-cell">{{ item.recorder }}</div>
-              <div class="table-cell">{{ item.date }}</div>
-              <div class="table-cell">{{ item.ifProved }}</div>
+          <div class="table-container">
+            <div class="table">
+              <div class="table__title">
+                <div class="table-cell">項目名稱</div>
+                <div class="table-cell">發表者</div>
+                <div class="table-cell">發表時間</div>
+                <div class="table-cell">狀態</div>
+              </div>
+              <div v-for="item in filteredItems" :key="item.id" class="table-row" @click="showItemcontent(item)">
+                <div class="table-cell">{{ item.title }}</div>
+                <div class="table-cell">{{ item.recorder }}</div>
+                <div class="table-cell">{{ item.date }}</div>
+                <div class="table-cell">{{ item.ifProved }}</div>
+              </div>
             </div>
           </div>
         </div>
   
-        <div id="Itemcontent" v-if="selectItem">
+        <div class="Itemcontent" v-if="selectItem">
           <div class="header-row">
             <p class="form__title"><strong>審核列表</strong></p>
-            <button @click="goback" class="button3">返回</button>
+            <button @click="goback" class="button3">
+              <img src="/images/verify/verify_back.png" alt="返回" class="button3-image" />
+            </button>
           </div>
-          <hr class="form--separator">
+          <hr class="form--separator mt-1 mb-4">
           <form class="mt-4">
             <div class="mb-3">
               <p class="label"><strong>項目名稱: </strong>{{ selectItem.title }}</p>
@@ -145,102 +149,115 @@
   color: #fff;
   transform: scale(1.05);
 }
-    .main-container{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
 
-    .form__title{
-        font-size: 40px;
-        margin-top: 20px;
-    }
+.main-container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
 
-    .form--separator{
-        height: 1%;
-        border-top: solid #000000;
-        margin-top: 1px;
-        margin-bottom: 10px;
-    }
+.form__title{
+    font-size: 40px;
+    margin-bottom: 15px;
+}
 
-    .table{
-        display: table;
-        width: 100%;
-        text-align: center;
-    }
+.form--separator{
+    height: 1%;
+    border-top: solid #000000;
+    margin-bottom: 10px;
+}
 
-    .table__title{
-        display: table-header-group;
-        font-weight: bold;
-        font-size: 30px;
-    }
+.table-container{
+  max-height: 400px;
+  overflow-y: auto;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
 
-    .table-row{
-        display: table-row;
-        cursor: pointer;
-    }
+.table-container::-webkit-scrollbar{
+  display: none;
+}
 
-    .table-row:hover{
-        background-color: rgba(196, 239, 239, 0.667)    
-    }
+.table{
+    display: table;
+    width: 100%;
+    text-align: center;
+}
 
-    .table-cell{
-        display: table-cell;
-        padding: 8px;
-        border-bottom: 1px solid #add;
-    }
+.table__title{
+    display: table-header-group;
+    font-weight: bold;
+    font-size: 30px;
+}
 
-    .table-cell:last-child{
-        text-align: center;
-    }
+.table-row{
+    display: table-row;
+    cursor: pointer;
+}
 
-    .header-row{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        width: 100%;
-    }
+.table-row:hover{
+    background-color: rgba(196, 239, 239, 0.667)    
+}
 
-    .label{
-        font-size: 30px;
-        margin-left: 12%;
-    }
+.table-cell{
+    display: table-cell;
+    padding: 8px;
+    border-bottom: 1px solid #add;
+}
 
-    .button1{
-        position: absolute;
-        top: 550px;
-        left: 400px;
-        width: 130px;
-        text-align: center;
-        font-size: 30px;
-        color: white;
-        background: #6FD586;
-        border-radius: 15px;
-    }
+.table-cell:last-child{
+    text-align: center;
+}
 
-    .button2{
-        position: absolute;
-        top: 550px;
-        left: 700px;
-        width: 130px;
-        text-align: center;
-        font-size: 30px;
-        color: white;
-        background: #E64C4C;
-        border-radius: 15px;
-    }
+.header-row{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+}
 
-    .button3{
-        width: 100px;
-        text-align: center;
-        font-size: 20px;
-        color: white;
-        background: #007bff;
-        border-radius: 15px;
-        border: none;
-        cursor: pointer;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+.label{
+    font-size: 30px;
+    margin-left: 12%;
+}
+
+.button1{
+    position: absolute;
+    top: 550px;
+    left: 400px;
+    width: 130px;
+    text-align: center;
+    font-size: 30px;
+    color: white;
+    background: #6FD586;
+    border-radius: 15px;
+}
+
+.button2{
+    position: absolute;
+    top: 550px;
+    left: 700px;
+    width: 130px;
+    text-align: center;
+    font-size: 30px;
+    color: white;
+    background: #E64C4C;
+    border-radius: 15px;
+}
+
+.button3{
+    width: 50px;
+    text-align: center;
+    opacity: 0.7;
+    border: none;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.button3-image{
+  width: 50px;
+  height: 50px;
+}
 </style>
