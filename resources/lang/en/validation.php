1e8c0d345e8ddb3633acf667fa83b692c2573975
yunnnnchieh <120265058+yunnnnchieh@users.noreply.github.com>
2024-05-26 00:21:14 +0800

    add register error handle

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -85,7 +85,7 @@ return [
     'max' => [
         'numeric' => 'The :attribute must not be greater than :max.',
         'file' => 'The :attribute must not be greater than :max kilobytes.',
-        'string' => 'The :attribute must not be greater than :max characters.',
+        'string' => '用戶名請輸入小於 :max 個字',
         'array' => 'The :attribute must not have more than :max items.',
     ],
     'mimes' => 'The :attribute must be a file of type: :values.',
@@ -93,7 +93,7 @@ return [
     'min' => [
         'numeric' => 'The :attribute must be at least :min.',
         'file' => 'The :attribute must be at least :min kilobytes.',
-        'string' => 'The :attribute must be at least :min characters.',
+        'string' => '密碼請設定 :max 位以上',
         'array' => 'The :attribute must have at least :min items.',
     ],
     'multiple_of' => 'The :attribute must be a multiple of :value.',
