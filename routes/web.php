2a01880b69cf6cc4bfd78e27823c57853bf4b7ee
eating <eva930514@gmail.com>
2024-05-27 22:58:28 +0800

    finish similar titles

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::get('/verify', function () {
 
 Route::get('/addpost', function () {
     return view('addpost');
-});//->middleware('auth')//
+})->middleware('auth');
 
 Route::get('/profile', function () {
     return view('profile');
