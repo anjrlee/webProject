27128998ed1962db28e095f037d2dc5819fb01ec
v1c999 <v1ct0riiaach0u@gmail.com>
2024-05-30 23:23:00 +0800

    post.vue

--- resources/js/components/post/Post.vue
+++ resources/js/components/post/Post.vue
@@ -2,64 +2,41 @@
   <div class="container mt-[100px] z-[0] absolute">
     <div class="title"> 
       <div class="title-background"></div>
-      <h1 class="title_text">{{ recordItem.title }}</h1>
+      <h1 class="title_text">{{ recordItem.title}}</h1>
     </div>
-    
-    <div class="main-content">
-      <div class="image-section">
-        <img :src="recordItem.cover" alt="Cover Image" class="image">
-      </div>
-      
-      <div class="award-section">
-        <h2 class="award-title">得獎感言:</h2>
-        <p class="award-speech">{{ recordItem.awardSpeech }}</p>
-      </div>
+    <div class="image-section">
+      <img :src="recordItem.cover" alt="Cover Image" class="image">
     </div>
-
     <div class="info-section">
-      <div class="info-item">
-        <img class="icon" src="/images/post/0.png">
-        <h2 class="info-title">發文者:</h2>
-        <router-link :to="'/userprofile/' + recordItem.user_id">
-          <div class="router-sec">
-            <p>{{ recordItem.username }}</p>
-          </div>
-        </router-link>
-      </div>
-      
-      <div class="info-item">
-        <img class="icon" src="/images/post/1.png">
-        <h2 class="info-title">紀錄保持人:</h2>
-        <p class="info-detail">{{ recordItem.recorder }}</p>
-      </div>
-      
-      <div class="info-item">
-        <img class="icon" src="/images/post/2.png">
-        <h2 class="info-title">紀錄:</h2>
-        <p class="info-detail">{{ recordItem.recordScore }}</p>
-      </div>
-      
-      <div class="info-item">
-        <img class="icon" src="/images/post/3.png">
-        <h2 class="info-title">完成日期:</h2>
-        <p class="info-detail">{{ recordItem.date }}</p>
-      </div>
-      
-      <div class="info-item">
-        <img class="icon" src="/images/post/4.png">
-        <h2 class="info-title">種類:</h2>
-        <p class="info-detail">{{ recordItem.type }}</p>
-      </div>
+      <img class="icon" src="/images/post/0.png">
+      <h2>發文者:</h2><br>
+      <router-link :to="'/userprofile/' + recordItem.user_id" class="info-detail">
+        <p>{{ recordItem.username }}</p>
+      </router-link>
+      <img class="icon" src="/images/post/1.png">
+      <h2>紀錄保持人: </h2><br>
+      <p class="info-detail">{{ recordItem.recorder }}</p><br>
+      <img class="icon" src="/images/post/2.png">
+      <h2>紀錄: </h2><br>
+      <p class="info-detail">{{ recordItem.recordScore }}</p><br>
+      <img class="icon" src="/images/post/3.png">
+      <h2>完成日期: </h2><br>
+      <p class="info-detail"> {{ recordItem.date }}</p><br>
+      <img class="icon" src="/images/post/4.png">
+      <h2>種類: </h2><br>
+      <p class="info-detail">{{ recordItem.type }}</p><br>
     </div>
+    <div class="award-section">
+      <h2>得獎感言: </h2><br>
+      <p>{{ recordItem.awardSpeech }}</p><br>
+    </div>
+  
   </div>
 </template>
-
-
   
 <script>
 import axios from 'axios';
 import { useRoute } from 'vue-router';
-
 export default {
   data() {
     return {
@@ -78,25 +55,34 @@ export default {
     };
   },
   methods: {
-    async fetchRecordItem() {
-      const id = this.$route.params.id;
-
-      try {
-        const response = await axios.get(`/api/post/${id}`);
-        const record = response.data;
-        this.recordItem = response.data;
-        this.recordItem.cover = record.cover || '/images/recordImg/default.jpg';
-        // Fetch user's name based on user_id
-        this.fetchUserName(this.recordItem.user_id);
-      } catch (error) {
-        console.error('Failed to fetch record item:', error);
-      }
-},
-
-
-    async fetchUserName(userId) {
-      try {
-        const response = await axios.get(`/api/userprofile/${userId}`);
+    getTypeLabel(type) {
+      switch (type) {
+        case 'study':
+          return '學術研究類';
+        case 'leisure':
+          return '休閒類';
+        case 'campus':
+          return '校園生活類';
+        default:
+          return '';
+       }
+     },
+     async fetchRecordItem() {
+       const id = this.$route.params.id;
+       try {
+         const response = await axios.get(`/api/post/${id}`);
+         const record = response.data;
+         this.recordItem = response.data;
+         this.recordItem.cover = record.cover || '/images/recordImg/default.jpg';
+         // Fetch user's name based on user_id
+         this.fetchUserName(this.recordItem.user_id);
+       } catch (error) {
+         console.error('Failed to fetch record item:', error);
+       }
+ },
+     async fetchUserName(userId) {
+       try {
+         const response = await axios.get(`/api/userprofile/${userId}`);
         console.log('User profile data:', response.data);
         this.recordItem.username = response.data.name;
         this.recordItem.id = response.data.user_id;
@@ -105,7 +91,6 @@ export default {
       }
     }
   },
-
   mounted() {
     this.fetchRecordItem();
   }
@@ -113,117 +98,69 @@ export default {
 </script>
   
 <style scoped>
-.container {
+.container{
   left: 50%;
   transform: translate(-50%, 0%);
-  max-width: 1200px;
-  padding: 20px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  background-color: #fff;
 }
-
+  
 .title {
-  position: relative;
+  position: relative; /* 使得內容可以相對於標題容器定位 */
   margin-top: 20px;
   margin-bottom: 50px;
+  border: 2px solid gray;
   word-wrap: break-word;
-  text-align: center;
 }
-
-.title_text {
+.title_text{
+  width: 60%;
   margin: 0 auto;
   text-align: center;
 }
-
+  
 .title-background {
-  position: absolute;
+  position: absolute; /* 相對於標題容器定位 */
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background-image: url('/images/post/title_bk.png');
-  background-size: cover;
-  background-position: center;
-  border-radius: 10px;
-  opacity: 1;
-  z-index: -1;
+  background-image: url('/images/post/title_bk.png'); 
+  background-size: cover; /* 自動調整背景圖片大小以填滿容器 */
+  background-position: center; 
+  opacity: 1; /* 設置背景圖片透明度，根據需要調整 */
+  z-index: -1; /* 將背景圖片放到標題容器的底層 */
 }
-
-.main-content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.image-section {
-  width: 100%;
-  max-width: 800px;
-  border: 2px solid gray;
-  margin-bottom: 20px;
-}
-
-.image {
-  width: 100%;
-  max-height: 500px;
-  height: auto;
-  object-fit: cover;
-}
-
-.award-section {
-  width: 100%;
-  text-align: center;
-  border-top: 1px solid black;
-  padding-top: 20px;
-  margin-bottom: 40px;
-}
-
-.award-speech {
-  font-size: 20px;
-  margin-top: 10px;
-  text-align: left;
-}
-
+  
 .info-section {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
+  width: 40%;
+  float: right;
 }
-
-.info-item {
-  width: calc(50% - 20px);
-  margin: 10px;
-  padding: 20px;
-  border-radius: 10px;
-  text-align: center;
-  background-color: rgb(213, 199, 188);
+  
+.image-section {
+  float: left;
+  width: 50%;
+  border: 2px solid gray;
 }
-
-.icon {
+  
+.image {
+  width: 100%; 
+  max-height: 500px; 
+  height: auto;
+  object-fit: cover; /* 保持比例填滿容器 */
+}
+  
+.award-section {
+  clear: both;
+  margin-top: 50px;
+  border-top: 1px solid black; /* 添加分隔線 */
+  padding-top: 10px;
+}
+  
+.icon{
   max-width: 40px;
   height: auto;
-  margin-bottom: 10px;
+  float: left;
+  margin-right: 10px;
 }
-
-.info-title {
-  font-size: 25px;
-  margin-bottom: 10px;
-}
-
-.info-detail, .router-sec p {
-  font-size: 20px;
-  margin: 0;
-}
-
-.router-sec {
-  padding: 10px;
-  border-radius: 10px;
-  margin: 0 auto;
-  width: fit-content;
-}
-
-.award-title{
-  text-align: left;
-  font-size: 25px;
-  margin-bottom: 10px;
+.info-detail{
+  text-align: center;
 }
 </style>
