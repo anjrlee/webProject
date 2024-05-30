b06468f21aae88159ae7e785172de925b9f459ec
v1c999 <v1ct0riiaach0u@gmail.com>
2024-05-30 21:52:40 +0800

    edit post.vue

--- resources/js/components/post/Post.vue
+++ resources/js/components/post/Post.vue
@@ -2,39 +2,59 @@
   <div class="container mt-[100px] z-[0] absolute">
     <div class="title"> 
       <div class="title-background"></div>
-      <h1 class="title_text">{{ recordItem.title}}</h1>
+      <h1 class="title_text">{{ recordItem.title }}</h1>
     </div>
-    <div class="image-section">
-      <img :src="recordItem.cover" alt="Cover Image" class="image">
+    
+    <div class="main-content">
+      <div class="image-section">
+        <img :src="recordItem.cover" alt="Cover Image" class="image">
+      </div>
+      
+      <div class="award-section">
+        <h2 class="award-title">得獎感言:</h2>
+        <p class="award-speech">{{ recordItem.awardSpeech }}</p>
+      </div>
     </div>
 
     <div class="info-section">
-      <img class="icon" src="/images/post/0.png">
-      <h2>發文者:</h2><br>
-      <router-link :to="'/userprofile/' + recordItem.user_id" class="info-detail">
-        <p>{{ recordItem.username }}</p>
-      </router-link>
-      <img class="icon" src="/images/post/1.png">
-      <h2>紀錄保持人: </h2><br>
-      <p class="info-detail">{{ recordItem.recorder }}</p><br>
-      <img class="icon" src="/images/post/2.png">
-      <h2>紀錄: </h2><br>
-      <p class="info-detail">{{ recordItem.recordScore }}</p><br>
-      <img class="icon" src="/images/post/3.png">
-      <h2>完成日期: </h2><br>
-      <p class="info-detail"> {{ recordItem.date }}</p><br>
-      <img class="icon" src="/images/post/4.png">
-      <h2>種類: </h2><br>
-      <p class="info-detail">{{ recordItem.type }}</p><br>
+      <div class="info-item">
+        <img class="icon" src="/images/post/0.png">
+        <h2 class="info-title">發文者:</h2>
+        <router-link :to="'/userprofile/' + recordItem.user_id">
+          <div class="router-sec">
+            <p>{{ recordItem.username }}</p>
+          </div>
+        </router-link>
+      </div>
+      
+      <div class="info-item">
+        <img class="icon" src="/images/post/1.png">
+        <h2 class="info-title">紀錄保持人:</h2>
+        <p class="info-detail">{{ recordItem.recorder }}</p>
+      </div>
+      
+      <div class="info-item">
+        <img class="icon" src="/images/post/2.png">
+        <h2 class="info-title">紀錄:</h2>
+        <p class="info-detail">{{ recordItem.recordScore }}</p>
+      </div>
+      
+      <div class="info-item">
+        <img class="icon" src="/images/post/3.png">
+        <h2 class="info-title">完成日期:</h2>
+        <p class="info-detail">{{ recordItem.date }}</p>
+      </div>
+      
+      <div class="info-item">
+        <img class="icon" src="/images/post/4.png">
+        <h2 class="info-title">種類:</h2>
+        <p class="info-detail">{{ recordItem.type }}</p>
+      </div>
     </div>
-
-    <div class="award-section">
-      <h2>得獎感言: </h2><br>
-      <p>{{ recordItem.awardSpeech }}</p><br>
-    </div>
-  
   </div>
 </template>
+
+
   
 <script>
 import axios from 'axios';
@@ -58,18 +78,6 @@ export default {
     };
   },
   methods: {
-    getTypeLabel(type) {
-      switch (type) {
-        case 'study':
-          return '學術研究類';
-        case 'leisure':
-          return '休閒類';
-        case 'campus':
-          return '校園生活類';
-        default:
-          return '';
-      }
-    },
     async fetchRecordItem() {
       const id = this.$route.params.id;
 
@@ -105,73 +113,117 @@ export default {
 </script>
   
 <style scoped>
-
-.container{
+.container {
   left: 50%;
   transform: translate(-50%, 0%);
-}
-  
-.title {
-  position: relative; /* 使得內容可以相對於標題容器定位 */
-  margin-top: 20px;
-  margin-bottom: 50px;
-  border: 2px solid gray;
-  word-wrap: break-word;
+  max-width: 1200px;
+  padding: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
 }
 
-.title_text{
-  width: 60%;
+.title {
+  position: relative;
+  margin-top: 20px;
+  margin-bottom: 50px;
+  word-wrap: break-word;
+  text-align: center;
+}
+
+.title_text {
   margin: 0 auto;
   text-align: center;
 }
-  
+
 .title-background {
-  position: absolute; /* 相對於標題容器定位 */
+  position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background-image: url('/images/post/title_bk.png'); 
-  background-size: cover; /* 自動調整背景圖片大小以填滿容器 */
-  background-position: center; 
-  opacity: 1; /* 設置背景圖片透明度，根據需要調整 */
-  z-index: -1; /* 將背景圖片放到標題容器的底層 */
+  background-image: url('/images/post/title_bk.png');
+  background-size: cover;
+  background-position: center;
+  border-radius: 10px;
+  opacity: 1;
+  z-index: -1;
 }
-  
-.info-section {
-  width: 40%;
-  float: right;
+
+.main-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
-  
+
 .image-section {
-  float: left;
-  width: 50%;
+  width: 100%;
+  max-width: 800px;
   border: 2px solid gray;
+  margin-bottom: 20px;
 }
-  
+
 .image {
-  width: 100%; 
-  max-height: 500px; 
+  width: 100%;
+  max-height: 500px;
   height: auto;
-  object-fit: cover; /* 保持比例填滿容器 */
+  object-fit: cover;
 }
-  
+
 .award-section {
-  clear: both;
-  margin-top: 50px;
-  border-top: 1px solid black; /* 添加分隔線 */
-  padding-top: 10px;
+  width: 100%;
+  text-align: center;
+  border-top: 1px solid black;
+  padding-top: 20px;
+  margin-bottom: 40px;
 }
-  
-.icon{
+
+.award-speech {
+  font-size: 20px;
+  margin-top: 10px;
+  text-align: left;
+}
+
+.info-section {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.info-item {
+  width: calc(50% - 20px);
+  margin: 10px;
+  padding: 20px;
+  border-radius: 10px;
+  text-align: center;
+  background-color: rgb(213, 199, 188);
+}
+
+.icon {
   max-width: 40px;
   height: auto;
-  float: left;
-  margin-right: 10px;
+  margin-bottom: 10px;
 }
 
-.info-detail{
-  text-align: center;
+.info-title {
+  font-size: 25px;
+  margin-bottom: 10px;
 }
 
+.info-detail, .router-sec p {
+  font-size: 20px;
+  margin: 0;
+}
+
+.router-sec {
+  padding: 10px;
+  border-radius: 10px;
+  margin: 0 auto;
+  width: fit-content;
+}
+
+.award-title{
+  text-align: left;
+  font-size: 25px;
+  margin-bottom: 10px;
+}
 </style>
